9b8d3015330973c1d6e2db331d5098811c0ba2d7
nsshahhh <2019628064@isiswa.uitm.edu.my>
2021-07-06 15:39:19 +0000

    Another modification

--- GUI_forClient.py
+++ GUI_forClient.py
@@ -1,266 +1,45 @@
-import socket
-import tkinter as tk
-from tkinter import font
-from tkinter import ttk
-from tkinter import filedialog
-import time
-import threading
 import os
+import sys
+import time
+import base64
+import threading
+from PyQt5.QtCore import Qt
+from Client import Chat_Client
+from PyQt5 import QtCore, QtGui, QtWidgets
 
-class GUI:
-    
-    def __init__(self, ipaddr, port):
-        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server.connect((ipaddr, port))
+class loginWindow(QtWidgets.QDialog):
+    def __init__(self):
+        super(loginWindow, self).__init__()
+        self.setupUi()
 
-        self.Window = tk.Tk()
-        self.Window.withdraw()
+    def setupUi(self):
+        self.setObjectName("LoginWindow")
+        self.setStyleSheet("#LoginWindow{border-image:url(./images/style/login/login.png);}")
+        self.setWindowIcon(QtGui.QIcon("./images/style/icon.png"))
+        self.resize(432, 300)
 
-        self.login = tk.Toplevel()
+        self.loginButton = QtWidgets.QPushButton(self)      
+        self.loginButton.setGeometry(QtCore.QRect(118, 243, 220, 35))
+        self.loginButton.setObjectName("login")
+        self.loginButton.setStyleSheet("border-image:url(./images/style/login/loginbutton.png);")
+        self.loginButton.clicked.connect(self.loginButtonClicked)
 
-        self.login.title("Login")
-        self.login.resizable(width=False, height=False)
-        self.login.configure(width=400, height=350)
+        self.registerButton = QtWidgets.QPushButton(self)   
+        self.registerButton.setGeometry(QtCore.QRect(12, 250, 65, 25))
+        self.registerButton.setObjectName("register")
+        self.registerButton.setStyleSheet("border:none;") 
+        self.registerButton.setCursor(Qt.PointingHandCursor)
+        self.registerButton.clicked.connect(self.registerButtonClicked)
 
-        self.pls = tk.Label(self.login, 
-                            text="Please Login to a enter the GROUP", 
-                            justify=tk.CENTER,
-                            font="Helvetica 13 bold")
+        self.userName = QtWidgets.QLineEdit(self)       
+        self.userName.setGeometry(QtCore.QRect(118, 140, 220, 28))
+        self.userName.setObjectName("username")
+        self.userName.setPlaceholderText("USERNAME")
+        self.userName.setMaxLength(20)
 
-        self.pls.place(relheight=0.15, relx=0.2, rely=0.07)
-
-        self.userLabelName = tk.Label(self.login, text="USERNAME: ", font="Helvetica 13")
-        self.userLabelName.place(relheight=0.2, relx=0.1, rely=0.25)
-
-        self.userEntryName = tk.Entry(self.login, font="Helvetica 13")
-        self.userEntryName.place(relwidth=0.4 ,relheight=0.1, relx=0.35, rely=0.30)
-        self.userEntryName.focus()
-
-        self.roomLabelName = tk.Label(self.login, text="ROOM PASSWORD: ", font="Helvetica 13")
-        self.roomLabelName.place(relheight=0.2, relx=0.1, rely=0.40)
-
-        self.roomEntryName = tk.Entry(self.login, font="Helvetica 13", show="*")
-        self.roomEntryName.place(relwidth=0.4 ,relheight=0.1, relx=0.35, rely=0.45)
-        
-        self.go = tk.Button(self.login, 
-                            text="NEXT", 
-                            font="Helvetica 13 bold", 
-                            command = lambda: self.goAhead(self.userEntryName.get(), self.roomEntryName.get()))
-        
-        self.go.place(relx=0.35, rely=0.62)
-
-        self.Window.mainloop()
-
-    def goAhead(self, username, room_id=0):
-        self.name = username
-        self.server.Enter(str.encode(username))
-        time.sleep(0.1)
-        self.server.Enter(str.encode(room_id))
-        
-        self.login.destroy()
-        self.layout()
-
-        rcv = threading.Thread(target=self.receive) 
-        rcv.start()
-
-
-    def layout(self):
-        self.Window.deiconify()
-        self.Window.title("GROUP")
-        self.Window.resizable(width=False, height=False)
-        self.Window.configure(width=470, height=550, bg="#17202A")
-        self.chatBoxHead = tk.Label(self.Window, 
-                                    bg = "#17202A", 
-                                    fg = "#EAECEE", 
-                                    text = self.name , 
-                                    font = "Helvetica 13 bold", 
-                                    pady = 5)
-
-        self.chatBoxHead.place(relwidth = 1)
-
-        self.line = tk.Label(self.Window, width = 450, bg = "#ABB2B9") 
-		
-        self.line.place(relwidth = 1, rely = 0.07, relheight = 0.012) 
-		
-        self.textCons = tk.Text(self.Window, 
-                                width=20, 
-                                height=2, 
-                                bg="#17202A", 
-                                fg="#EAECEE", 
-                                font="Helvetica 13", 
-                                padx=5, 
-                                pady=5) 
-		
-        self.textCons.place(relheight=0.745, relwidth=1, rely=0.08) 
-		
-        self.labelBottom = tk.Label(self.Window, bg="#ABB2B9", height=80) 
-		
-        self.labelBottom.place(relwidth = 1, 
-							    rely = 0.8) 
-		
-        self.entryMsg = tk.Entry(self.labelBottom, 
-                                bg = "#2C3E50", 
-                                fg = "#EAECEE", 
-                                font = "Helvetica 13")
-        self.entryMsg.place(relwidth = 0.74, 
-							relheight = 0.03, 
-							rely = 0.008, 
-							relx = 0.013) 
-        self.entryMsg.focus()
-
-        self.buttonMsg = tk.Button(self.labelBottom, 
-								text = "Enter", 
-								font = "Helvetica 13 bold", 
-								width = 20, 
-								bg = "#ABB2B9", 
-								command = lambda : self.EnterButton(self.entryMsg.get())) 
-        self.buttonMsg.place(relx = 0.77, 
-							rely = 0.008, 
-							relheight = 0.03, 
-							relwidth = 0.22) 
-
-
-        self.labelFile = tk.Label(self.Window, bg="#ABB2B9", height=70) 
-		
-        self.labelFile.place(relwidth = 1, 
-							    rely = 0.9) 
-		
-        self.fileLocation = tk.Label(self.labelFile, 
-                                text = "Add File",
-                                bg = "#2C3E50", 
-                                fg = "#EAECEE", 
-                                font = "Helvetica 13")
-        self.fileLocation.place(relwidth = 0.65, 
-                                relheight = 0.03, 
-                                rely = 0.008, 
-                                relx = 0.013) 
-
-        self.Search = tk.Button(self.labelFile, 
-								text = "Search", 
-								font = "Helvetica 13 bold", 
-								width = 13, 
-								bg = "#ABB2B9", 
-								command = self.SearchFile)
-        self.Search.place(relx = 0.67, 
-							rely = 0.008, 
-							relheight = 0.03, 
-							relwidth = 0.15) 
-
-        self.sengFileBtn = tk.Button(self.labelFile, 
-								text = "Enter", 
-								font = "Helvetica 13 bold", 
-								width = 13, 
-								bg = "#ABB2B9", 
-								command = self.EnterFile)
-        self.sengFileBtn.place(relx = 0.84, 
-							rely = 0.008, 
-							relheight = 0.03, 
-							relwidth = 0.15)
-    
-
-        self.textCons.config(cursor = "arrow")
-        scrollbar = tk.Scrollbar(self.textCons) 
-        scrollbar.place(relheight = 1, 
-						relx = 0.974)
-
-        scrollbar.config(command = self.textCons.yview)
-        self.textCons.config(state = tk.DISABLED)
-
-
-    def SearchFile(self):
-        self.filename = filedialog.askopenfilename(initialdir="/", 
-                                    title="Choose a file",
-                                    filetypes = (("Text files", 
-                                                "*.txt*"), 
-                                                ("all files", 
-                                                "*.*")))
-        self.fileLocation.configure(text="File Opened: "+ self.filename)
-
-    def EnterFile(self):
-        self.server.Enter("FILE".encode())
-        time.sleep(0.1)
-        self.server.Enter(str("client_" + os.path.basename(self.filename)).encode())
-        time.sleep(0.1)
-        self.server.Enter(str(os.path.getsize(self.filename)).encode())
-        time.sleep(0.1)
-
-        file = open(self.filename, "rb")
-        data = file.read(1024)
-        while data:
-            self.server.Enter(data)
-            data = file.read(1024)
-        self.textCons.config(state=tk.DISABLED)
-        self.textCons.config(state = tk.NORMAL)
-        self.textCons.insert(tk.END, "<You> "
-                                     + str(os.path.basename(self.filename)) 
-                                     + " Sent\n\n")
-        self.textCons.config(state = tk.DISABLED) 
-        self.textCons.see(tk.END)
-
-
-    def EnterButton(self, msg):
-        self.textCons.config(state = tk.DISABLED) 
-        self.msg=msg 
-        self.entryMsg.delete(0, tk.END) 
-        snd= threading.Thread(target = self.EnterMessage) 
-        snd.start() 
-
-
-    def receive(self):
-        while True:
-            try:
-                message = self.server.recv(1024).decode()
-
-                if str(message) == "FILE":
-                    file_name = self.server.recv(1024).decode()
-                    lenOfFile = self.server.recv(1024).decode()
-                    Enter_user = self.server.recv(1024).decode()
-
-                    if os.path.exists(file_name):
-                        os.remove(file_name)
-
-                    total = 0
-                    with open(file_name, 'wb') as file:
-                        while str(total) != lenOfFile:
-                            data = self.server.recv(1024)
-                            total = total + len(data)     
-                            file.write(data)
-                    
-                    self.textCons.config(state=tk.DISABLED)
-                    self.textCons.config(state = tk.NORMAL)
-                    self.textCons.insert(tk.END, "<" + str(Enter_user) + "> " + file_name + " Received\n\n")
-                    self.textCons.config(state = tk.DISABLED) 
-                    self.textCons.see(tk.END)
-
-                else:
-                    self.textCons.config(state=tk.DISABLED)
-                    self.textCons.config(state = tk.NORMAL)
-                    self.textCons.insert(tk.END, 
-                                    message+"\n\n") 
-
-                    self.textCons.config(state = tk.DISABLED) 
-                    self.textCons.see(tk.END)
-
-            except: 
-                print("An error has occured!") 
-                self.server.close() 
-                break
-
-    def EnterMessage(self):
-        self.textCons.config(state=tk.DISABLED) 
-        while True:  
-            self.server.Enter(self.msg.encode())
-            self.textCons.config(state = tk.NORMAL)
-            self.textCons.insert(tk.END, 
-                            "<You> " + self.msg + "\n\n") 
-
-            self.textCons.config(state = tk.DISABLED) 
-            self.textCons.see(tk.END)
-            break
-
-
-
-if __name__ == "__main__":
-    ip_address = "127.0.0.1"
-    port = 12345
-    g = GUI(ip_address, port)
+        self.password = QtWidgets.QLineEdit(self)   
+        self.password.setGeometry(QtCore.QRect(118, 170, 220, 28))
+        self.password.setObjectName("password")
+        self.password.setPlaceholderText("PASSWORD")
+        self.password.setMaxLength(20)
+        self.password.setEchoMode(self.password.Password)
